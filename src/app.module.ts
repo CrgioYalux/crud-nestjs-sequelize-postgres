ad9da274d77d1ec7cd1489f9a1e56ed20759027d
Sergio Yalux <randaledomman1@gmail.com>
2022-08-21 21:57:27 -0300

    feat: import task module to app base module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthorModule } from './author/author.module';
+import { TaskModule } from './task/task.module';
 @Module({
-  imports: [DatabaseModule, AuthorModule],
+  imports: [DatabaseModule, AuthorModule, TaskModule],
   controllers: [],
   providers: [],
 })
