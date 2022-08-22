28864223c976f15b8d4eb93bbb9806e4a2223a0a
Sergio Yalux <randaledomman1@gmail.com>
2022-08-21 21:56:44 -0300

    feat: add task model to database

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -2,6 +2,7 @@ import { AppConstant } from "src/app.constant";
 import { Sequelize } from 'sequelize-typescript';
 import { Config } from './database.config';
 import { Author } from '../author/author.model';
+import { Task } from '../task/task.model';
 
 export const databaseProvider = [
     {
@@ -9,20 +10,24 @@ export const databaseProvider = [
         useFactory: async () => {
             const config = Config();
             const sequelize = new Sequelize(
-                {
-                  dialect: 'postgres',
-                  host: config.DB.HOST,
-                  port: config.DB.PORT,
-                  database: config.DB.NAME,
-                  username: config.DB.USER.NAME,
-                  password: config.DB.USER.PASS,
-                  define: {
-                    freezeTableName: true
-                  }
+              {
+                dialect: 'postgres',
+                host: config.DB.HOST,
+                port: config.DB.PORT,
+                database: config.DB.NAME,
+                username: config.DB.USER.NAME,
+                password: config.DB.USER.PASS,
+                define: {
+                  freezeTableName: true
                 }
+              }
             );
 
-            sequelize.addModels([Author]);
+            sequelize.addModels([Author, Task]);
+
+            Task.belongsTo(Author, {
+              as: 'author'
+            });
 
             await sequelize.sync({ force: true });
 
